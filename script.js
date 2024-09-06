c89a724396cb22cae2d763f69dfdf25014883070
camille emel < ser.email>
2024-09-06 14:09:51 +0200

    mb done ??

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
 console.log(`${cities.length} communes chargées`);
+/***
+ const cities =
+ {"code":"01001","nom":"L'Abergement-Clémenciat",
+ "population":767,"codesPostaux":["01400"],
+ "codeDepartement":"01"}
+
+ ***/
+
+
+let largeCityPop = cities
+    .filter(city => city.population > 300000)
+    //sort avant car int -> string !!!
+    .sort((a,b) => a.population - b.population)
+    .map(city => city.nom + " " + city.population);
+
+console.log(largeCityPop);
